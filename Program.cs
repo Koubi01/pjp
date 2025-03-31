dacd2d9cc200022c312c5d28a6fcb48a955d658a
Koubi01 <koubi01@seznam.cz>
2025-03-31 20:08:33 +0200

    add everything

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Váš kód zde
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
