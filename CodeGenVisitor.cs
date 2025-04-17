7a88f0452acf410c11f5bc6b0a16768e8452671b
Koubi01 <koubi01@seznam.cz>
2025-04-17 14:12:59 +0200

    CodeGenDone podruhe

--- CodeGenVisitor.cs
+++ CodeGenVisitor.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using Antlr4.Runtime;
 using pjpproject;
 
-//public enum PType { Int, Float, Bool, String, Error }
-
 public class CodeGenVisitor : PLCBaseVisitor<PType>
 {
     private readonly Dictionary<string, PType> _symbolTable = new();
@@ -44,7 +42,6 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
         foreach (var id in context.ID())
         {
             _symbolTable[id.GetText()] = type;
-           // _instructions.Add($"VisitVariableDecl {id.GetText()}");
             _instructions.Add($"push {GetTypeCode(type)} {GetDefaultValue(type)}");
             _instructions.Add($"save {id.GetText()}");
         }
@@ -68,25 +65,22 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
         var text = context.literal().GetText();
         if (context.literal().INT() != null)
         {
-            //_instructions.Add($"VisitLiteralExpr ");
             _instructions.Add($"push I {text}");
             return PType.Int;
         }
         if (context.literal().FLOAT() != null)
         {
-            //_instructions.Add($"VisitLiteralExpr ");
             _instructions.Add($"push F {text}");
             return PType.Float;
         }
         if (context.literal().STRING() != null)
         {
-            //_instructions.Add($"VisitLiteralExpr ");
             _instructions.Add($"push S {text}");
             return PType.String;
         }
         if (text == "true" || text == "false")
         {
-           // _instructions.Add($"VisitLiteralExpr ");
+
             _instructions.Add($"push B {text}");
             return PType.Bool;
         }
@@ -98,7 +92,7 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
         var name = context.ID().GetText();
         if (_symbolTable.TryGetValue(name, out var type))
         {
-            //_instructions.Add($"VisitIdExpr ");
+
             _instructions.Add($"load {name}");
             return type;
         }
@@ -116,16 +110,16 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
 
         if (leftType == PType.Float && rightType == PType.Int)
         {
-            //_instructions.Add($"VisitAssignExpr ");
+
             _instructions.Add("itof");
         }
-        //_instructions.Add($"VisitAssignExpr ");
+
         _instructions.Add($"save {name}");
         _instructions.Add($"load {name}");
 
         if (context.Parent is not PLCParser.AssignExprContext)
         {
-            //_instructions.Add($"VisitAssignExpr ");            
+   
             _instructions.Add("pop");
         }
 
@@ -144,7 +138,7 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
         {
             if (leftType == PType.String || rightType == PType.String)
             {
-                //_instructions.Add($"VisitAddExpr ");
+
                 _instructions.Add("concat");
                 return PType.String;
             }
@@ -153,13 +147,13 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
                  (rightType == PType.Int || rightType == PType.Float))
         {
             if (leftType == PType.Float && rightType == PType.Int){
-               // _instructions.Add($"VisitAddExprFloat-INT ");
+
                 _instructions.Add("itof");}
             if (leftType == PType.Int && rightType == PType.Float){
-               // _instructions.Add($"VisitAddExprINT-Float ");
+
                 _instructions.Insert(_instructions.Count - 1, "itof");}
                 
-            //_instructions.Add($"VisitAddExpr ");
+
             _instructions.Add(op switch
             {
                 "+" => $"add {GetTypeCode(PromoteType(leftType, rightType))}",
@@ -186,13 +180,13 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
             (rightType == PType.Int || rightType == PType.Float))
         {
             if (leftType == PType.Float && rightType == PType.Int){
-              //  _instructions.Add($"VisitMulExprFloat-INT ");
+
                 _instructions.Add("itof");}
             if (leftType == PType.Int && rightType == PType.Float){
-              //  _instructions.Add($"VisitMulExprINT-Float ");
+
                 _instructions.Insert(_instructions.Count - 1, "itof");}
 
-           //_instructions.Add($"VisitMulExpr ");
+
             _instructions.Add(op switch
             {
                 "*" => $"mul {GetTypeCode(PromoteType(leftType, rightType))}",
@@ -215,7 +209,6 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
             Visit(expr);
             exprCount++;
         }
-        //_instructions.Add($"VisitWriteStmt ");
         _instructions.Add($"print {exprCount}");
         return PType.Error;
     }
@@ -227,7 +220,6 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
             var name = id.GetText();
             if (_symbolTable.TryGetValue(name, out var type))
             {
-            //    _instructions.Add($"VisitReadStmt ");
                 _instructions.Add($"read {GetTypeCode(type)}");
                 _instructions.Add($"save {name}");
             }
@@ -239,113 +231,128 @@ public class CodeGenVisitor : PLCBaseVisitor<PType>
     {
         return (a == PType.Float || b == PType.Float) ? PType.Float : PType.Int;
     }
-
-    public override PType VisitRelExpr(PLCParser.RelExprContext context)
-        {
-            var left = Visit(context.expression(0));
-            var right = Visit(context.expression(1));
-            if (AreBothNumeric(left, right))
-                return PType.Bool;
-
-            Errors.ReportError(context.Start, "Relational operators require numeric operands.");
-            return PType.Error;
-        }
-
-        public override PType VisitEqExpr(PLCParser.EqExprContext context)
-        {
-            var left = Visit(context.expression(0));
-            var right = Visit(context.expression(1));
-
-            //Console.WriteLine($"[DEBUG] VisitEqExpr: left={left}, right={right}");
-
-            if (left == PType.Error || right == PType.Error)
-            {
-                return PType.Error;
-            }              
-
-            if (left == right || AreBothNumeric(left, right))
-            {
-                return PType.Bool;
-            }
-
-            Errors.ReportError(context.Start, $"Cannot compare {left} and {right}.");
-            return PType.Error;
-        }
-
-        public override PType VisitOrExpr(PLCParser.OrExprContext context)
-        {
-            return CheckBoolBinary(context, "||");
-        }
-
-        public override PType VisitAndExpr(PLCParser.AndExprContext context)
-        {
-            return CheckBoolBinary(context, "&&");
-        }
-
-        public override PType VisitNotExpr(PLCParser.NotExprContext context)
-        {
-            //Console.WriteLine($"[DEBUG] VisitNotExpr subtree: {context.GetText()}");
-            var operand = Visit(context.expression());
-            //Console.WriteLine($"[DEBUG] VisitNotExpr operand type: {operand}");
-
-            if (operand != PType.Bool)
-            {
-                Errors.ReportError(context.Start, "Operator '!' requires a boolean operand.");
-                return PType.Error;
-            }
-
-            return PType.Bool;
-        }
-
-        public override PType VisitIfStmt(PLCParser.IfStmtContext context)
-        {
-            var condType = Visit(context.expression());
-            if (condType != PType.Bool)
-                Errors.ReportError(context.expression().Start, "Condition in 'if' must be boolean.");
-
-            Visit(context.statement(0));
-            if (context.statement().Length > 1)
-                Visit(context.statement(1));
-
-            return PType.Error;
-        }
-
-        public override PType VisitWhileStmt(PLCParser.WhileStmtContext context)
-        {
-            var condType = Visit(context.expression());
-            if (condType != PType.Bool)
-                Errors.ReportError(context.expression().Start, "Condition in 'while' must be boolean.");
-
-            Visit(context.statement());
-            return PType.Error;
-        }
-
-        public override PType VisitBlock(PLCParser.BlockContext context)
+    public override PType VisitBlock(PLCParser.BlockContext context)
         {
             foreach (var stmt in context.statement())
                 Visit(stmt);
             return PType.Error;
         }
-         private static bool IsAssignable(PType target, PType value)
+
+        public override PType VisitRelExpr(PLCParser.RelExprContext context)
         {
-            if (target == value) return true;
-            if (target == PType.Float && value == PType.Int) return true;
-            return false;
+            var leftType = Visit(context.expression(0));
+            var rightType = Visit(context.expression(1));
+            var op = context.GetChild(1).GetText();
+            string typeCode = GetTypeCode(leftType);
+
+            if (leftType == PType.Int && rightType == PType.Float){
+                _instructions.Insert(_instructions.Count - 1, "itof");
+                typeCode = GetTypeCode(PromoteType(leftType, rightType));}
+            if (leftType == PType.Float && rightType == PType.Int){
+                _instructions.Insert(_instructions.Count - 2, "itof");
+                typeCode = GetTypeCode(PromoteType(leftType, rightType));}
+          
+            if (op == "<")
+                _instructions.Add($"lt {typeCode}");
+            else if (op == ">")
+                _instructions.Add($"gt {typeCode}");
+
+            return PType.Bool;
         }
 
-        private PType CheckBoolBinary(ParserRuleContext context, string op)
-        {
-            var left = Visit(context.GetRuleContext<PLCParser.ExpressionContext>(0));
-            var right = Visit(context.GetRuleContext<PLCParser.ExpressionContext>(1));
-            if (left == PType.Bool && right == PType.Bool)
-                return PType.Bool;
 
-            Errors.ReportError(context.Start, $"Operator '{op}' requires boolean operands.");
+        public override PType VisitEqExpr(PLCParser.EqExprContext context)
+        {
+            var leftType = Visit(context.expression(0));
+            var rightType = Visit(context.expression(1));
+            var op = context.GetChild(1).GetText();
+            string typeCode = GetTypeCode(leftType);
+
+            if (leftType == PType.Int && rightType == PType.Float){
+                _instructions.Insert(_instructions.Count - 1, "itof");
+                 typeCode = GetTypeCode(PromoteType(leftType, rightType));}
+            if (leftType == PType.Float && rightType == PType.Int){
+                _instructions.Insert(_instructions.Count - 2, "itof");
+                 typeCode = GetTypeCode(PromoteType(leftType, rightType));}
+
+           
+            _instructions.Add($"eq {typeCode}");
+
+            if (op == "!=")
+                _instructions.Add("not");
+
+            return PType.Bool;
+        }
+
+
+        public override PType VisitOrExpr(PLCParser.OrExprContext context)
+        {
+            Visit(context.expression(0));
+            Visit(context.expression(1));
+            _instructions.Add("or");
+            return PType.Bool;
+        }
+
+        public override PType VisitAndExpr(PLCParser.AndExprContext context)
+        {
+            Visit(context.expression(0));
+            Visit(context.expression(1));
+            _instructions.Add("and");
+            return PType.Bool;
+        }
+
+
+        public override PType VisitNotExpr(PLCParser.NotExprContext context)
+        {
+            Visit(context.expression());
+            _instructions.Add("not");
+            return PType.Bool;
+        }
+
+
+        private int _labelCounter = 0;
+
+       public override PType VisitIfStmt(PLCParser.IfStmtContext context)
+        {
+            int elseLabel = _labelCounter++;
+            int endLabel = _labelCounter++;
+
+            Visit(context.expression());
+            _instructions.Add($"fjmp {elseLabel}");
+
+            Visit(context.statement(0));
+
+            if (context.statement().Length > 1)
+            {
+                _instructions.Add($"jmp {endLabel}");
+                _instructions.Add($"label {elseLabel}");
+                Visit(context.statement(1)); // else-block
+                _instructions.Add($"label {endLabel}");
+            }
+            else
+            {
+                _instructions.Add($"jmp {endLabel}");
+                _instructions.Add($"label {elseLabel}");
+                _instructions.Add($"label {endLabel}");
+            }
+
             return PType.Error;
         }
-         private static bool AreBothNumeric(PType a, PType b)
-        {
-            return (a == PType.Int || a == PType.Float) && (b == PType.Int || b == PType.Float);
-        }
 
+
+        public override PType VisitWhileStmt(PLCParser.WhileStmtContext context)
+        {
+            int startLabel = _labelCounter++;
+            int endLabel = _labelCounter++;
+
+            _instructions.Add($"label {startLabel}");
+            Visit(context.expression()); 
+            _instructions.Add($"fjmp {endLabel}");
+
+            Visit(context.statement()); 
+            _instructions.Add($"jmp {startLabel}");
+            _instructions.Add($"label {endLabel}");
+
+            return PType.Error;
+        }
 }
